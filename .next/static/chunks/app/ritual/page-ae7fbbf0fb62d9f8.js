e70a46cb0e59fe2eef9a93861e19a2cdddc08535
tehreemejaz382 <tehreemejaz382@gmail.com>
2026-05-24 14:37:24 +0500

    Update project files

--- .next/static/chunks/app/ritual/page-ae7fbbf0fb62d9f8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[433,329,521,649],{7323:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=7323)}),_N_E=n.O()}]);
