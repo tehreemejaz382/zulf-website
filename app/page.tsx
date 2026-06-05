1be8a6be01e863186bba179d6c9f1e3b94ff2fc9
tehreemejaz382 <tehreemejaz382@gmail.com>
2026-06-05 23:14:43 +0500

    mob

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function ZulfHomepage() {
               <img
                 src={heroSlides[currentSlide]}
                 alt="ZULF Hair Elixir"
-                className="w-full h-full object-cover"
+                className="w-full h-full object-contain md:object-cover"
               />
             </motion.div>
           </AnimatePresence>
